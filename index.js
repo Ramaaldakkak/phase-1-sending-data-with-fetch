3fb4992c3741bac222525d3d73106e5b437ca429
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-09 10:00:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+fetch('http://localhost:3000/users', {
+    method: 'POST',
+
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+
+    body: JSON.stringify({name, email})
+  }
+      .then((response) => response.json())
+
+      .then((object) => console.log(object))
+
+      .catch((error) => {
+        alert("Bad things! Ragnarők!")
+        console.log(error.message)
+}) 
+)}
+
+submitData("Fleabag", "fleabagisnormal@smth.com")
